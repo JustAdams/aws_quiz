8fa337d032c4d6efaedec8e15680a44ac9cab154
Justin Adams <AdamsJustin@protonmail.com>
2024-11-17 10:06:20 -0600

    Improved GUI layout
    Better grid layout design

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
 use rand::prelude::IndexedRandom;
 use serde::Deserialize;
-use slint::SharedString;
+use std::fs;
 
 slint::include_modules!();
 
@@ -14,7 +13,7 @@ struct QuizQuestion {
     question: String,
     answers: Vec<String>,
     correct_answer: usize,
-    explanation: Vec<String>
+    explanation: Vec<String>,
 }
 
 fn main() {
@@ -27,38 +26,40 @@ fn main() {
 
     let mut counter = 0;
 
-        // grab a random question
-        let mut rng = rand::thread_rng();
-        let question = quiz_clone.choose(&mut rng).expect("No questions found");
+    // grab a random question
+    let mut rng = rand::thread_rng();
+    let question = quiz_clone.choose(&mut rng).expect("No questions found");
 
-        cloned_main_window.set_question(question.question.clone().into());
-        cloned_main_window.set_answer_0(question.answers[0].clone().into());
-        cloned_main_window.set_answer_1(question.answers[1].clone().into());
-        cloned_main_window.set_answer_2(question.answers[2].clone().into());
-        cloned_main_window.set_answer_3(question.answers[3].clone().into());
+    cloned_main_window.set_question(question.question.clone().into());
+    cloned_main_window.set_answer_0(question.answers[0].clone().into());
+    cloned_main_window.set_answer_1(question.answers[1].clone().into());
+    cloned_main_window.set_answer_2(question.answers[2].clone().into());
+    cloned_main_window.set_answer_3(question.answers[3].clone().into());
 
-        let mut question_clone = question.clone();
-        let cloned_main_window_inner = cloned_main_window.clone_strong();
+    let mut question_clone = question.clone();
+    let cloned_main_window_inner = cloned_main_window.clone_strong();
 
-        cloned_main_window.on_select_answer(move |selected_index: i32| {
-            cloned_main_window_inner.set_explanation(question_clone.explanation[selected_index as usize].clone().into());
-            if selected_index == question_clone.correct_answer as i32 {
-                cloned_main_window_inner.set_counter(cloned_main_window_inner.get_counter() + 1);
-                // move to the next question
-                //main_window.set_explanation(question_clone.explanation[selected_index as usize].clone().into());
-            
-                let mut rng = rand::thread_rng();
-                let question = quiz_clone.choose(&mut rng).expect("No questions found");
-                question_clone = question.clone();
-                cloned_main_window_inner.set_question(question.question.clone().into());
-                cloned_main_window_inner.set_answer_0(question.answers[0].clone().into());
-                cloned_main_window_inner.set_answer_1(question.answers[1].clone().into());
-                cloned_main_window_inner.set_answer_2(question.answers[2].clone().into());
-                cloned_main_window_inner.set_answer_3(question.answers[3].clone().into());
-            } else {
-                cloned_main_window_inner.set_counter(0);
-            }
-        });
+    cloned_main_window.on_select_answer(move |selected_index: i32| {
+        cloned_main_window_inner.set_explanation(question_clone.explanation[selected_index as usize].clone().into());
+        if selected_index == question_clone.correct_answer as i32 {
+            cloned_main_window_inner.set_counter(cloned_main_window_inner.get_counter() + 1);
+            cloned_main_window_inner.set_progress(cloned_main_window_inner.get_counter() as f32 / quiz_questions.len() as f32);
+            // move to the next question
+            //main_window.set_explanation(question_clone.explanation[selected_index as usize].clone().into());
+
+            let mut rng = rand::thread_rng();
+            let question = quiz_clone.choose(&mut rng).expect("No questions found");
+            question_clone = question.clone();
+            cloned_main_window_inner.set_question(question.question.clone().into());
+            cloned_main_window_inner.set_answer_0(question.answers[0].clone().into());
+            cloned_main_window_inner.set_answer_1(question.answers[1].clone().into());
+            cloned_main_window_inner.set_answer_2(question.answers[2].clone().into());
+            cloned_main_window_inner.set_answer_3(question.answers[3].clone().into());
+        } else {
+            cloned_main_window_inner.set_counter(0);
+            cloned_main_window_inner.set_progress(0.0);
+        }
+    });
 
     main_window.run().expect("Issue encountered while running the UI");
 }
